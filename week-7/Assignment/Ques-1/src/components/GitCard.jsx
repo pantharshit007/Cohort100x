758c20e7ebbc37e2859f5c0afe902e47e26ced7f
pantharshit007 <20BCS7140@cuchd.in>
2024-02-25 16:18:21 +0530

    corrected the api calls

--- week-7/Assignment/Ques-1/src/components/GitCard.jsx
+++ week-7/Assignment/Ques-1/src/components/GitCard.jsx
@@ -1,43 +1,46 @@
 import React from 'react';
 
 function GitCard(props) {
+  // console.log(name);
+
+  const { name, avatar_url, login, html_url, public_repos, followers, following } = props.userData || {};
   return (
     <div className='w-[400px] h-[500px] bg-gray-200 relative flex flex-col justify-between rounded-lg'>
       <div>
-        <img src= {(props.avatar_url) ? props.avatar_url : "https://source.unsplash.com/user/c_v_r/1600x900"} alt="Free unsplash image" className="rounded-t-lg" />
+        <img src= {"https://source.unsplash.com/user/c_v_r/1600x900"} alt="Free unsplash image" className="rounded-t-lg" />
       </div>
 
       <div className='absolute left-[35%] top-[30%]'>
         <img
-          src="https://i.pinimg.com/736x/eb/93/06/eb93060a6952932727507cf2c65665dc.jpg"
+          src={(avatar_url) ? avatar_url : "https://i.pinimg.com/736x/eb/93/06/eb93060a6952932727507cf2c65665dc.jpg"}
           alt=""
           className='rounded-full h-[130px] w-[130px]'
         />
       </div>
 
       <div className='grid place-items-center h-[100px]'>
-        <span>Harshit Pant{props.name}</span>
-        <p><a href={props.html_url}>
-            @{ (props.name) ?
-            props.login : 'pantharshit007'
+        <span className='text-2xl text-black font-bold'>{name}</span>
+        <p className='text-xl text-gray-700'><a href={html_url}>
+            @{ (name) ?
+            login : 'pantharshit007'
             }</a>
         </p>
       </div>
 
       <div className="flex mx-auto  items-center text-center h-[15%] mb-2 border-t border-black ">
         <div className='border-r border-black px-7'>
-          <h2>{props.public_repos}</h2>
-          <span>Repository</span>
+          <h2 className='text-2xl text-black font-bold'>{public_repos}</h2>
+          <span className='text-xs text-gray-800'>Repository</span>
         </div>
 
         <div className='border-r border-black px-7'>
-          <h2>{props.followers}</h2>
-          <span>Followers</span>
+          <h2 className='text-2xl text-black font-bold'>{followers}</h2>
+          <span className='text-xs text-gray-800'>Followers</span>
         </div>
 
         <div className=' px-7'>
-          <h2>{props.following}</h2>
-          <span>Following</span>
+          <h2 className='text-2xl text-black font-bold'>{following}</h2>
+          <span className='text-xs text-gray-800'>Following</span>
         </div>
 
       </div>
